b5be126272370aab400cdb56d29c122e3b348188
Your Name <dfsilvat@gmail.com>
2023-02-08 22:01:40 -0300

    carousel

---
+++ scripts/carrousel.js
@@ -0,0 +1,15 @@
+const carousel = document.getElementById('carousel')
+let count = 0;
+
+setInterval(() => {
+    if(count >= 0 && count  <= 3 ){
+        count = count + 1
+    }else{
+        count = 1
+    }
+
+    carousel.style.backgroundImage = `url(../src/img/hotel${count}.jpg)`
+
+    console.log(count)
+}, 5000)
+console.log(`count: ${count}`)
